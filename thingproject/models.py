abb2beda80efbbdfbef8df9a4994d8a3afe193e7
Joel Jolly <joel.jolly@kcl.ac.uk>
2022-12-09 11:55:40 +0000

    final

--- thingproject/models.py
+++ thingproject/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Thing(models.Model):
+class Thing(AbstractUser):
     name = models.CharField(unique= True, blank=False, max_length=30)
     description = models.CharField(blank=True, max_length=120)
     quantity = models.IntegerField(validators=[
