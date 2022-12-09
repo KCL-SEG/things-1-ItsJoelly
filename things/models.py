02ca027c50ea3bbab4c14d66547336edd37042fe
Joel Jolly <joel.jolly@kcl.ac.uk>
2022-12-09 15:08:50 +0000

    folder change

--- things/models.py
+++ things/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-
-class Thing(AbstractUser):
-    name = models.CharField(unique= True, blank=False, max_length=30)
-    description = models.CharField(blank=True, max_length=120)
-    quantity = models.IntegerField(validators=[
-            MaxValueValidator(100),
-            MinValueValidator(0)
-        ])
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=120,blank=True)
+    quantity = models.PositiveIntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(100)])
